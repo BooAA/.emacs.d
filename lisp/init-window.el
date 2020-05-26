7fe426cf20fa35cff7d253001508921143258652
BooAA <booaa@debian>
2020-05-27 01:59:25 +0800

    lisp/init-window.el: use custom keyword to setup tab-bar-mode
    Emacs bug about using customize-set-variable to setup
    tab-bar-select-tab-modifiers seems to be fixed.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -18,11 +18,9 @@
 
 (use-package tab-bar
   :ensure nil
-  ;; using customize-set-variable will trigger tab-bar-mode to  restart,
-  ;; we don't wan't that, since minibuffer will disappear with unknown reason
-  :init (setq tab-bar-select-tab-modifiers '(control super))
   :custom ((tab-bar-show nil)
-           (tab-bar-new-tab-choice "*scratch*"))
+           (tab-bar-new-tab-choice "*scratch*")
+           (tab-bar-select-tab-modifiers '(control super)))
   :hook (after-init . tab-bar-mode))
 
 (provide 'init-window)
