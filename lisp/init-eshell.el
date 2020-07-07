44b864a5ae449421a61f4ee9fe270e8920d02c15
BooAA <booaa@debian>
2020-07-07 23:12:08 +0800

    rename init-eshell to init-shell

--- lisp/init-eshell.el
+++
@@ -1,9 +0,0 @@
-;;; -*- lexical-binding: t -*-
-
-(use-package eshell
-  :ensure nil
-  :custom ((eshell-prefer-lisp-functions t)
-           (eshell-prefer-lisp-variables t))
-  :config (add-to-list 'eshell-modules-list 'eshell-tramp))
-
-(provide 'init-eshell)
