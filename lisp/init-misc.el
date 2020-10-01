3e13452faaf14b827fa04c3bf895904983a1a2fb
BooAA <s930054123yaoyao@gmail.com>
2020-10-01 15:11:17 +0800

    lisp/init-misc.el: Add youtube-dl, install manually from github.

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -8,4 +8,9 @@
 (use-package olivetti
   :custom (olivetti-body-width 80))
 
+(use-package youtube-dl
+  :load-path "site-lisp/youtube-dl-emacs"
+  :custom (youtube-dl-directory "~/Downloads")
+  :commands (youtube-dl youtube-dl-playlist youtube-dl-list))
+
 (provide 'init-misc)
