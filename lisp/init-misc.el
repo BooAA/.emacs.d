bfc166d360bb6b370d7cf99b4cf895de51e86ad9
BooAA <s930054123yaoyao@gmail.com>
2020-11-21 22:49:06 +0800

    lisp/init-misc.el: Set youtube-dl-slow-rate to bigger number.

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -10,7 +10,8 @@
 
 (use-package youtube-dl
   :load-path "site-lisp/youtube-dl-emacs"
-  :custom (youtube-dl-directory "~/Downloads")
+  :custom ((youtube-dl-directory "~/Downloads")
+           (youtube-dl-slow-rate "10M"))
   :commands (youtube-dl youtube-dl-playlist youtube-dl-list))
 
 (use-package exwm
