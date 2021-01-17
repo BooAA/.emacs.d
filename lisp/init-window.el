dae7fdde4ba07a20589c574a9d0ccaa827073c5f
BooAA <s930054123yaoyao@gmail.com>
2021-01-17 19:50:40 +0800

    lisp/init-window.el: Fix wrong indentation.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -19,10 +19,10 @@
 
 (use-package tab-bar
   :ensure nil
-   :init (custom-set-variables '(tab-bar-show nil))
-   :custom (;; (tab-bar-show nil)
+  :init (custom-set-variables '(tab-bar-show nil))
+  :custom (;; (tab-bar-show nil)
            (tab-bar-new-tab-choice "*scratch*")
            (tab-bar-select-tab-modifiers '(control super)))
-   :hook (after-init . tab-bar-mode))
+  :hook (after-init . tab-bar-mode))
 
 (provide 'init-window)
