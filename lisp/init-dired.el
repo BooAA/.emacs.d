8b6ae48775b6f25d18b4a3ed00ec686bc97e6351
BooAA <s930054123yaoyao@gmail.com>
2021-05-01 00:27:09 +0800

    lisp/init-dired.el: Fix indentation.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -3,8 +3,8 @@
 (use-package dired
   :ensure nil
   :custom ((dired-dwim-target t)
-	         (dired-recursive-copies 'always)
-	         (dired-recursive-deletes 'always))
+	   (dired-recursive-copies 'always)
+	   (dired-recursive-deletes 'always))
   :config (put 'dired-find-alternate-file 'disabled nil))
 
 (provide 'init-dired)
