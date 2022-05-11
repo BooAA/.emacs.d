f9659fdcb0406a7bc3e78dd9ddd5c5723e884feb
BooAA <s930054123yaoyao@gmail.com>
2022-05-11 21:00:54 +0800

    lisp/init-search.el: Cleanup up unused bindings. Fix wrong type bug.

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -6,11 +6,12 @@
       lazy-highlight-initial-delay 0.1
       isearch-resume-in-command-history t)
 
-(setq xref-search-program "ripgrep"
+(setq xref-search-program 'ripgrep
       xref-truncation-width 100
       xref-after-jump-hook '(xref-pulse-momentarily))
 
-(define-key search-map (kbd "O") #'multi-occur-in-matching-buffers)
+(use-package wgrep)
+
 (define-key search-map (kbd "g") #'lgrep)
 (define-key search-map (kbd "G") #'rgrep)
 
